8c342666c5007bb0348a3b0cdc7a2be0ee4a7e4b
XIETONGJIAN <583110978@qq.com>
2025-03-12 23:51:34 +0800

    feat:增加审批时手写签名的功能。

---
+++ apps/web-antd/src/views/components/common/widgets/esign/SignModal.vue
@@ -0,0 +1,69 @@
+<template>
+  <BasicModal title="编辑签名" class="w-[800px] min-h-[400px]" >
+    <div class="h-full flex flex-row border border-3 mb-2 border-dashed">
+      <VueEsign
+        class="h-full flex-1"
+        ref="vueSignRef"
+        :width="800"
+        height="300"
+        :isCrop="false"
+        :lineWidth="6"
+        :lineColor="'#000000'"
+        v-model:bgColor="bgColor" />
+    </div>
+    <div class="text-center">
+      <Button danger @click="handleReset">清空画板</Button>
+    </div>
+  </BasicModal>
+</template>
+<script lang="ts" setup>
+  import { ref, unref, defineExpose, defineEmits } from 'vue';
+  import {useVbenModal} from '@vben/common-ui';
+  import VueEsign from 'vue-esign'
+  import {Button} from 'ant-design-vue';
+
+  const emit = defineEmits(['success']);
+
+  const bgColor = ref();
+  const vueSignRef = ref();
+  const resultImg = ref();
+  const [BasicModal, modalApi] = useVbenModal({
+    draggable: true,
+    fullscreenButton: false,
+    onCancel() {
+      modalApi.close();
+    },
+    onOpenChange(isOpen: boolean) {
+      if (isOpen) {
+      }
+    },
+    onConfirm() {
+      handleSubmit();
+    },
+  });
+
+  function handleReset () {
+    vueSignRef.value.reset();
+  }
+
+  async function handleGenerate () {
+    try{
+      const res = await vueSignRef.value.generate();
+      resultImg.value = res
+    } catch (e){
+      console.error(e);
+    }
+  }
+
+  async function handleSubmit() {
+    try {
+      await handleGenerate();
+      emit('success', resultImg.value);
+      modalApi.close();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  defineExpose(modalApi);
+</script>
---
+++ apps/web-antd/src/views/components/common/widgets/esign/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="flex items-center h-8">
+      <span v-if="!!signImg" >
+        <img :src="signImg" alt="签名图片" class="w-25 h-8 border-1"/>
+      </span>
+      <TypographyLink @click="editSign">
+        <EditOutlined />
+      </TypographyLink>
+    </div>
+    <SignModal ref="signModalRef" @success="signSuccess" />
+  </div>
+</template>
+<script lang="ts" setup>
+import {ref, defineProps, defineEmits, nextTick, defineExpose} from 'vue';
+import {Space, Popover, Spin, Button, message, TypographyLink} from 'ant-design-vue';
+import {EditOutlined, PaperClipOutlined} from '@ant-design/icons-vue'
+import SignModal from './SignModal.vue';
+const signImg = ref('');
+
+const signModalRef = ref();
+
+function editSign() {
+  signModalRef.value.open();
+  signModalRef.value.setData({});
+}
+
+function signSuccess(imgBase64) {
+  signImg.value = imgBase64;
+}
+
+</script>
