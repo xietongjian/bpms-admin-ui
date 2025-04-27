350a4675e32ec1e4828fe582a149531da2ac666f
XIETONGJIAN <583110978@qq.com>
2025-04-27 23:52:50 +0800

    fix:功能优化。

--- apps/web-antd/src/components/selector/index.ts
+++ apps/web-antd/src/components/selector/index.ts
@@ -1,8 +1,4 @@
 export { default as OrgSelector } from './org-selector/org-selector.vue';
-// export { default as PersonalSelector } from './org-selector/org-selector.vue';
 export { default as PersonalSelector } from './personal-selector/index.vue';
 export { default as FlowModelSelector } from './flow-model-selector/index.vue';
-
-
-// export { default as PersonalSelector } from './personal-selector/index.vue';
 // export { default as PersonalSelectorModal } from './personal-selector/FlowModelSelectorModal.vue';
